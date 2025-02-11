03a39b8c68ea96e1612f15631837656e0b6eb0d5
Ankita3648 <ankitapatil642003@gmail.com>
2025-02-11 09:28:00 +0530

    Update Hello1.py

--- Hello1.py
+++ Hello1.py
@@ -2,3 +2,8 @@ a=int(input("Enter the first number"))
 b=int(input("Enter the second number"))
 sub=a-b
 print("Sub of the two numbers is:",sub)
+
+a=int(input("Enter the first number"))
+b=int(input("Enter the second number"))
+sum=a+b
+print("Sum of the two numbers is:",sum)
